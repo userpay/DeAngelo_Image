f3f9d7b1d2094f89e12f35c8cd56e47508f4edd8
userpay <mdeangelo@csumb.edu>
2014-03-10 15:21:04 -0700

    Addition 2
    Fixing how program reads in pixel values.

--- image.py
+++ image.py
@@ -1,5 +1,5 @@
 import Image, winsound
-im = Image.open("toei_sonic_16_bit_by_somesortofrobot-d327sgh.jpg")
+im = Image.open("colorful_flower_frame-33px.jpg")
 pix = im.load()
 picsize = im.size #Get the width and hight of the image for iterating over
 print picsize
@@ -10,19 +10,35 @@ lowestpixel = 255
 highestpixel = 0
 rangepixel = 0
 pixeldivision = 1
-
+pixel = 1
+#pixtup = 0
+pixtotal = 0
+tuplength = 0
 
 for i in range(width):
     for j in range(height):
         x = i
         y = j
-        if pix[x,y] != 255:
-            print pix[x,y] #Get the RGBA Value of the a pixel of an image
-            if pix[x,y] < lowestpixel: #checks what lowest pixel thus far is
-                lowestpixel = pix[x,y]
-            if pix[x,y] > highestpixel: #checks what highest pixel thus far is except for 255
-                highestpixel = pix[x,y]
-        elif pix[x,y] == 255:
+        g = 0
+        pixel = 0
+        if type(pix[x,y]) == tuple:
+            pixtotal = 0
+            tuplength = len(pix[x,y])
+            pixtup = pix[x,y]
+            while g < tuplength:
+                pixel = pixtup[g] + pixel
+                g = g + 1
+            pixtotal = pixel/tuplength
+        else:
+            pixtotal = pix[x,y]
+        print pixtotal
+        if pixtotal != 255:
+            print pixtotal #Get the RGBA Value of the a pixel of an image
+            if pixtotal < lowestpixel: #checks what lowest pixel thus far is
+                lowestpixel = pixtotal
+            if pixtotal > highestpixel: #checks what highest pixel thus far is except for 255
+                highestpixel = pixtotal
+        elif pixtotal == 255:
             count = count + 1
 
 #counts how many pixels wern't outputted. Said pixels having a value of 255
@@ -48,15 +64,16 @@ for i in range(width):
     for j in range(height):
         x = i
         y = j
-        if pix[x,y] != 255:
-            print pix[x,y] #Get the RGBA Value of the a pixel of an image
-            if pix[x,y] <= first:
+        if pixtotal != 255:
+            print pixtotal #Get the RGBA Value of the a pixel of an image
+            if pixtotal <= first:
                 winsound.PlaySound("SystemAsterisk", winsound.SND_ALIAS)
-            elif first < pix[x,y] <= second:
+            elif first < pixtotal <= second:
                 winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
-            elif second < pix[x,y] <= third:
+            elif second < pixtotal <= third:
                 winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
-            elif third < pix[x,y] <= fourth:
+            elif third < pixtotal <= fourth:
                 winsound.PlaySound("SystemHand", winsound.SND_ALIAS)
-            elif fourth < pix[x,y] <= fifth:
+            elif fourth < pixtotal <= fifth:
                 winsound.PlaySound("SystemQuestion", winsound.SND_ALIAS)
+
